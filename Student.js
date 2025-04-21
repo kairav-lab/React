392999c792229edbea45d40411b2410b06639e4f
kairav-lab <k49455058@gmail.com>
2025-04-21 15:04:17 +0530

    Update Student.js

--- Student.js
+++ Student.js
@@ -7,3 +7,14 @@ function Student(props){
   )
 }
 export default Student;
+
+function Student(props){
+  console.log(props)
+  return(
+    <div style={{backgroundColor: "skyblue",margin:10}}>
+      <h1>Hello {props.name}</h1>
+      <h2>Email: {props.email}</h2>
+    </div>
+  )
+}
+export default Student;
