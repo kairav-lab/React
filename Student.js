daec53781d7d8022212f35e3abe64cc8695f8eff
kairav-lab <k49455058@gmail.com>
2025-04-21 19:23:20 +0530

    Update Student.js

--- Student.js
+++ Student.js
@@ -18,3 +18,16 @@ function Student(props){
   )
 }
 export default Student;
+
+import React from 'react'
+export default class Student extends React.Component{
+    render(){
+        console.log(this.props)
+        return(
+            <div style={{backgroundColor:'skyblue',margin:20}}>
+              <h1>Hello {this.props.name}</h1>
+              <h3>{this.props.email}</h3>
+            </div>
+        )
+    }
+}
