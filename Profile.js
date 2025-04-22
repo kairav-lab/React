9d307cfec2726e573659337d46631d730355f2b6
kairav-lab <k49455058@gmail.com>
2025-04-22 09:54:11 +0530

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+function Profile(){
+  const [loggedIn,setLoggedIn]=useState(truecm)
+  if(loggedIn){
+    return (
+      <div>
+        {loggedIn==1?<h1>Welcome User 1</h1>:loggedIn==2?<h1>Welcome User 2</h1>:<h1>Welcome Guest</h1>
+      </div>
+    );
+  }
+}
+export default Profile;
